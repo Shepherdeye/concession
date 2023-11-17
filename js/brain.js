e036e9f1a42dba5a982cd34f5db1c57271adedef
Shepherd's Eye <134965007+Shepherdeye@users.noreply.github.com>
2023-11-17 14:53:27 +0200

    Update brain.js

--- js/brain.js
+++ js/brain.js
@@ -1,14 +1,16 @@
-﻿let day = document.getElementById("days"),
+let day = document.getElementById("days"),
   gbyte = document.getElementById("gbytes"),
   Cases = document.getElementById("Cases"),
   check = document.getElementById("check"),
   row = document.getElementById("row"),
   less = document.getElementById("less"),
+
   more = document.getElementById("more"),
   moreThan = document.getElementById("moreThan"),
 
   lessThan = document.getElementById("lessThan"),
-
+  lessThan2 = document.getElementById("lessThan2"),
+  copyText = document.querySelector("#copy"),
 
   result = document.getElementById("result"),
   duration = document.getElementById("duration"),
@@ -29,13 +31,21 @@ check.onclick = function () {
 less.onclick = function () {
   // console.log("sayed")
   lessThan.classList.remove("hidden1");
+  lessThan2.classList.remove("hidden1");
+  copyText.classList.remove("hidden1");
+
+  
 }
 more.onclick = function () {
   // console.log("sayed")
   moreThan.classList.remove("hidden1");
 }
 
-
+copyText.onclick=function(){
+  navigator.clipboard.writeText(lessThan2.innerText);
+  this.classList.add("changeme")
+  
+}
 
 
 
